1859a494c784ce2c7bc817239e5ce1503c2964cb
Cinderace <55834323+Zensos@users.noreply.github.com>
2022-07-15 18:10:34 +0700

    Update firebase.js

--- src/plugins/firebase.js
+++ src/plugins/firebase.js
@@ -2,13 +2,13 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCqnOvXXk6RJKrHNpyE2r0h8fDs9k85OIs",
-    authDomain: "webtopia-itcamp.firebaseapp.com",
-    projectId: "webtopia-itcamp",
-    storageBucket: "webtopia-itcamp.appspot.com",
-    messagingSenderId: "945410318606",
-    appId: "1:945410318606:web:1bf63020cdabf3c914775f",
-    measurementId: "G-70GWSMV0C2"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 const app = firebase.initializeApp(firebaseConfig).firestore();
